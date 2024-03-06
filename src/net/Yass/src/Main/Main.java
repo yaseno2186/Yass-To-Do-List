51104ebaacdb525b39bd786ec37ba7d6af4cd63e
Yass Almardod <149265907+yaseno2186@users.noreply.github.com>
2024-03-06 01:05:07 +0100

    check it out!

--- src/net/Yass/src/Main/Main.java
+++ src/net/Yass/src/Main/Main.java
@@ -4,26 +4,31 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static Scanner inputs = new Scanner(System.in);
-    public static Scanner inputsNum = new Scanner(System.in);
 
+
+    public static Scanner inputsNum = new Scanner(System.in);
     public static boolean dare(String YorN) {
         return YorN.contains("y") || YorN.contains("+");
     }
-    private static final ArrayList<String> tasks = new ArrayList<>(5);
+    private static final ArrayList<String> taskTitle = new ArrayList<>(5);
+    private static final ArrayList<String> taskDescription = new ArrayList<>(5);
     public static void main(String[] args) {
+        Scanner inputs = new Scanner(System.in);
         for (int i = 0; i < 5; i++) {
+
             System.out.println("Do you want to write To-do's ?");
             boolean addTask = dare(inputs.nextLine());
             if (addTask) {
                 System.out.println("Enter what to add on the To-do List: ");
-                tasks.add(inputs.nextLine());
-                System.out.println("You add on the to do list: " + tasks.get(i));
+                taskTitle.add(inputs.nextLine());
+                System.out.println("You add on the to do list: " + taskTitle.get(i));
+                System.out.println("Enter description: ");
+                taskDescription.add(inputs.nextLine());
                 printList();
             } else {
                 printList();
                 System.out.println("Enter a task number to delete: ");
-                tasks.remove(inputsNum.nextInt() -1);
+                taskTitle.remove(inputsNum.nextInt() -1);
                 System.out.println("Deleted it successfully");
                 printList();
                 break;
@@ -32,8 +37,8 @@ public class Main {
     }
     public static void printList(){
         System.out.println("Your current list is: ");
-        for (int j = 0; j < tasks.size(); j++) {
-            System.out.println(j + 1 + ". " + tasks.get(j).toUpperCase());
+        for (int j = 0; j < taskTitle.size(); j++) {
+            System.out.println(j + 1 + ". " + taskTitle.get(j).toUpperCase()+"\n\t"+taskDescription.get(j).toLowerCase());
         }
     }
 
